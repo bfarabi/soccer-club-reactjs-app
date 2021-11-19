2f78da4fa3572bd0cc93ba58257f021302fd171b
Billal Farabi <bfarabi108@gmail.com>
2021-11-19 08:06:13 +0600

    final

--- src/Components/FriendsDetails/FriendsDetails.js
+++ src/Components/FriendsDetails/FriendsDetails.js
@@ -1,25 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 
 const FriendsDetails = () => {
     const {fndId} = useParams();
     const [friend, setFriend] = useState({});
+    let navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
   useEffect(() => {
      fetch(`https://jsonplaceholder.typicode.com/users/${fndId}`)
     .then(response => response.json())
     .then(data => setFriend(data))
-  }, []);
-  debugger;
+  }, [fndId]);
+  
+        const singleFriendStyle = {
+            border:'1px solid grey',
+            margin:'10px',
+            padding:'10px',
+        }
     return (
-        <div>
-            <p>friend details No-{fndId}</p>
+        <div style={singleFriendStyle}>
+            <p>Friend details No-{fndId}</p>
             <h1>Name: {friend.name}</h1>
             <h3>Nickname: {friend.username} </h3>
             <p>Email: {friend.email} </p>
             <p >Website: {friend.website}</p>
             <p>Phone: {friend.phone}</p>
-            <p>Address: </p>
+            <button onClick={goBack} >Go Back</button>
+            
 
         </div>
     );
